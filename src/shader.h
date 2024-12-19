d7ebc42e8263e88d14d27a234912eb8e32930380
shalin_ <shalindahiya@gmail.com>
2024-12-19 15:58:21 +0100

    add shader class

---
+++ src/shader.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <GL/glew.h>
+
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+// Opengl Shader abstraction class
+class Shader
+{
+public:
+	Shader(const std::string& vertexShader, const std::string& fragmentShader);
+	~Shader();
+
+	void Bind() const;
+	void Unbind() const;
+	void SetUniform1i(const std::string& name, int v);
+	void SetUniform3f(const std::string& name, float v0, float v1, float v2);
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+
+	static std::vector<std::string> ShaderDirectories;
+private:
+	ShaderProgramSource ParseShader() const;
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+
+	int GetUniformLocation(const std::string& name) const;
+
+private:
+	unsigned int m_ID;
+	std::string m_VSFilePath;
+	std::string m_FSFilePath;
+};
