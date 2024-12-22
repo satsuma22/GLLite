9cd0e8bc674506a6e7739b28564604c551f98e5d
shalin_ <shalindahiya@gmail.com>
2024-12-22 15:00:33 +0100

    add camera class

--- GLLite/include/camera.h
+++ GLLite/include/camera.h
@@ -1,3 +1,58 @@
 #pragma once
 
 #include <glm.hpp>
+
+#include "utils.h"
+
+class GLLITE_API Camera
+{
+public:
+    enum MovementType
+    {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+        UP_SPIN,
+        DOWN_SPIN,
+        LEFT_SPIN,
+        RIGHT_SPIN,
+    };
+
+public:
+    Camera();
+    Camera(glm::vec3 pos, glm::vec3 dir, glm::vec3 worldup);
+
+    inline const glm::mat4& GetViewMatrix() const { return m_View; }
+    inline const glm::mat4& GetProjectionMatrix() const { return m_Projection; }
+
+    glm::vec3 GetPosition() const { return m_Position; }
+    glm::vec3 GetProjectedPosition() const;
+
+    void MoveCamera(MovementType type, float delta);
+
+private:
+    void UpdateViewMatrix();
+    void UpdateProjectionMatrix();
+    void Update();
+
+public:
+    glm::mat4 m_View;
+    glm::mat4 m_Projection;
+
+    glm::vec3 m_Position;
+    glm::vec3 m_Forward;
+    glm::vec3 m_Right;
+    glm::vec3 m_Up;
+    glm::vec3 m_WorldUp;
+
+    float m_Speed;
+    float m_RotationSpeed;
+    float m_FOV;
+    float m_Height;
+    float m_Width;
+    float m_NearClip;
+    float m_FarClip;
+};
